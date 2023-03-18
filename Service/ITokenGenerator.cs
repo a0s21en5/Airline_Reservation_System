15aafcaccf1754fe69715ca2f31ab366d9c93397
Ashish Sahu <ashsahu9713@gmail.com>
2023-03-18 21:57:31 +0530

    Update

--- Service/ITokenGenerator.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Airline_Reservation_System.Service
-{
-    public interface ITokenGenerator
-    {
-        string GenerateToken(int userId, string userName);
-        bool IsTokenValid(string userSecretKey, string userIssuer, string userAudience, string userToken);
-    }
-}
